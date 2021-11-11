4c1f4f95126a78191bce34a9fe22538a1c1e9566
Bharadwaj <sbhatbharadwaj24@gmail.com>
2021-11-11 15:57:13 +0530

    deploying

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
-  <Router basename="/Sorting_Algorithm_Visualizer/home">
+  <Router >
     <App />
   </Router>,
 
